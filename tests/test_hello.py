cd7b70af78f95a835d72dfa9546073246c5adee2
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2023-11-13 11:23:49 +0000

    Write hello() test, implement, pass tests

---
+++ tests/test_hello.py
@@ -0,0 +1,15 @@
+from lib.hello import *
+
+def test_hello_sarah_returns_hello_sarah():
+    expected = 'Hello, Sarah!'
+    actual = hello('Sarah')
+
+    assert actual == expected
+
+def test_hello_james_returns_hello_james():
+    expected = 'Hello, James!'
+    actual = hello('James')
+
+    assert actual == expected
+
+# we want to get 'Hello, Sarah!' back when called hello('Sarah') 
